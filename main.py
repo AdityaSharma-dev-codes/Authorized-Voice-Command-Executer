f9b216b342642c865e25b0d87ff8cd60b555592a
Aditya Sharma <aditya29032005@gmail.com>
2026-03-14 15:42:56 +0530

    Added desktop notifications to indicate status when triggered via keyboard shortcut

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from voice_ver import verify_voice
 from speech_rec import recognize_speech
 from command_exec import exec_command
+import os
 
 def main():
+
+    os.chdir("/home/aditya/PycharmProjects/SpeechAI")
+    os.system('notify-send "Voice Assistant" "Listening..."')
+
     authenticated = verify_voice()
 
     if authenticated:
+        os.system('notify-send "Voice Assistant" "Authenticated"')
         text = recognize_speech("command.wav")
-        print("command: ", text)
         exec_command(text)
 
     else:
-        print("User not authorized")
+        os.system('notify-send "Voice Assistant" "Authentication Failed"')
 
 if __name__ == "__main__":
     main()
